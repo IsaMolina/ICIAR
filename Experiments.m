0437696d3abab9f74f1f38b858b6b9f424cd3785
IsaMolina <48458614+IsaMolina@users.noreply.github.com>
2019-03-15 21:38:35 -0400

    Add files via upload

--- Experiments.m
+++ Experiments.m
@@ -1,5 +1,5 @@
 close all;
-A = (imread('cameraman.tif'));
+A = (imread('moon.tif'));
 
 A_noisy = (imnoise(A, 'gaussian', 0, 1*0.005));
 
@@ -8,22 +8,22 @@ lambda_2 = 100;
 u1=totalvar_noiterations(A_noisy, lambda_1);
 u2=totalvar_noiterations(A_noisy, lambda_2);
 figure; imshow(A); title('Original');
-figure; imshow(A_noisy); title('Noisyinput');
+figure; imshow(A_noisy); title('Noisy input');
 figure; imshow(u1); title(['lambda = ', num2str(lambda_1)]);
 figure; imshow(u2); title(['lambda = ', num2str(lambda_2)]);
 
-%B = imread('Original.jpg');
-%C = imread('Noisyinput.jpg');
-%D = imread('lambdasmall.jpg');
-%E = imread('lambdabig.jpg');
+B = imread('Original.jpg');
+C = imread('Noisy input.jpg');
+D = imread('lambda 0.01.jpg');
+E = imread('lambda 100.jpg');
 
-%figure;
-%subplot(121); imshow(B);
-%subplot(122); imshow(C);
+figure;
+subplot(121); imshow(B);
+subplot(122); imshow(C);
 
-%figure;
-%subplot(121); imshow(D);
-%subplot(122); imshow(E);
+figure;
+subplot(121); imshow(D);
+subplot(122); imshow(E);
 
 ssim(A_noisy,A)
 ssim(u1,A)
